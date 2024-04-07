76ae53b1e5d723c83642a40acb6f19bf67b56283
Kaeli Dalton <Kaelidalton@ufl.edu>
2024-04-07 10:52:46 -0400

    hamburger menu attempt

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -2,9 +2,15 @@ import Link from "next/link"
 import useLogout from '../../hooks/useLogout'
 import styles from './style.module.css'
 import Image from "next/image"
+import { useState } from "react"
 
 export default function Header(props){
     const logout = useLogout()
+    const [hamburgerOpen, setHamburgerOpen] = useState(false)
+
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen)
+    }
     return (
         <header className={styles.container}>
             {/* <img src='/myreads.png'>
@@ -14,25 +20,75 @@ export default function Header(props){
             <div className={styles.nav}>
                 {props.isLoggedIn ? (
                     <>
+                    <ul className={styles.ul}>
+                   <li className={styles.li}>
                     <Link href='/books' className={styles.Link}>Your List</Link>
+                    </li> 
+                    <li className={styles.li}>
                     <Link href='/note' className={styles.Link}>Your Notes</Link>
+                    </li>
+                    <li className={styles.li}>
                     <Link href='/search' className={styles.Link}>Search</Link>
+                    </li>
+                    <li className={styles.li}>
                     <a href="#" onClick={logout} className={styles.Link}>
                         Logout
                     </a>
+                    </li>
+
+                    </ul>
+                    <div className={styles.hamburger} onClick={toggleHamburger}>
+                        <Hamburger />
+                    </div>
                     </>
                 ) :(
                     <>
-                    <Link href='/search' className={styles.Link}> Search</Link>
+                     <ul className={styles.ul}>
+                         <li className={styles.li}>
+                            <Link href='/search' className={styles.Link}> Search</Link>
+                        </li>
+                        <li className={styles.li}>
                     <Link href='/login' className={styles.Link}>Login</Link>
+                    </li>
+                    <li className={styles.li}>
                     <Link href='/signup' className={styles.Link}>Sign Up</Link>
+                    </li>
+                    <li className={styles.li}>
                     <Link href='/' className={styles.Link}>Home</Link>
+                    </li>
+                    <li className={styles.li}>
                     <Link href='/books' className={styles.Link}>Your List</Link>
+                    </li>
+                    <li className={styles.li}>
                     <Link href='/note' className={styles.Link}>Your Notes</Link>
+                    </li>
+                    <li className={styles.li}>
                     <Link href='/create' className={styles.Link}>Create Note</Link>
+                    </li>
+                    </ul>
+                    <div className={styles.hamburger} onClick={toggleHamburger}>
+                        <Hamburger />
+                    </div>
                     </>
                 )}
             </div>
+            <style jsx>{`
+            .ul {
+                display: ${hamburgerOpen ? 'inline' : 'none'};
+            }
+            `}</style>
         </header>
+    )
+}
+
+export function Hamburger(){
+    return(
+        <>
+        <div className={styles.hamburger}>
+            <div className={styles.burger} />
+            <div className={styles.burger} />
+            <div className={styles.burger} />
+        </div>
+        </>
     )
 }
