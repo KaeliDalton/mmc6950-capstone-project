ad8c6238c40b1316ba209cfaac1f47878a73775a
Kaeli Dalton <Kaelidalton@ufl.edu>
2024-04-17 11:34:07 -0400

    auth tweaks

--- pages/api/auth/[action].js
+++ pages/api/auth/[action].js
@@ -1,6 +1,7 @@
 import {withIronSessionApiRoute} from 'iron-session/next'
 import sessionOptions from '../../../config/session'
 import db from '../../../db'
+import { use } from 'react'
 
 export default withIronSessionApiRoute(
     function handler(req, res){
@@ -23,11 +24,11 @@ export default withIronSessionApiRoute(
 async function login(req, res){
     const { username, password } = req.body
     try{
-        const {
-            password: _,
-            ...otherFields
-        } = await db.auth.login(username,password)
-        req.session.user = otherFields
+        const user = await db.auth.login(username,password)
+        req.session.user = {
+            username: user.username,
+            id: user.id
+        }
         await req.session.save()
         res.status(200).end()
     } catch(err){
@@ -41,11 +42,11 @@ async function logout(req, res){
 async function signup(req, res){
     try{
         const {username, password} = req.body
-        const {
-            password: _,
-            ...otherFields
-        } = await db.user.create(username, password)
-        req.session.user = otherFields
+        const user = await db.user.create(username, password)
+        req.session.user = {
+            username: user.username,
+            id: user.id
+        }
         await req.session.save()
         res.redirect('/')
     } catch(err){
