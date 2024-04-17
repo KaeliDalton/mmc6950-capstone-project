8a5bb5b4af3524de6cb1bb53390938dfa05f6e50
Kaeli Dalton <Kaelidalton@ufl.edu>
2024-04-17 19:00:13 -0400

    note troubleshoot

--- pages/note.jsx
+++ pages/note.jsx
@@ -4,36 +4,37 @@ import { withIronSessionSsr } from "iron-session/next";
 import sessionOptions from "../config/session";
 import db from "../db";
 import NoteList from "../components/noteList";
+import Link from 'next/link'
 import Footer from "../components/footer";
 import Header from "../components/header";
 
-// export const getServerSideProps = withIronSessionSsr(
-//   async function getServerSideProps({ req }) {
-//     const user = req.session.user;
-//     let notes
-//     if (user)
-//       notes = await db.note.getAll(user.id)
-//     if (!notes) {
-//       req.session.destroy()
-//       return {
-//         redirect: {
-//           destination: '/login',
-//           permanent: false
-//         }
-//       }
-//     }
-//     return {
-//       props: {
-//         user: req.session.user,
-//         isLoggedIn: true,
-//         notes: notes,
-//       }
-//     };
-//   },
-//   sessionOptions
-// );
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req }) {
+    const user = req.session.user;
+    let notes
+    if (user)
+      notes = await db.note.getAll(user.id)
+    if (!notes) {
+      req.session.destroy()
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false
+        }
+      }
+    }
+    return {
+      props: {
+        user: req.session.user,
+        isLoggedIn: true,
+        notes: notes,
+      }
+    };
+  },
+  sessionOptions
+);
 
-export default function Notes() {
+export default function Notes(props) {
   return (
     <>
       <Head>
@@ -41,13 +42,22 @@ export default function Notes() {
         <meta name="description" content="Your notes on MyReads" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      {/* <Header isLoggedIn={props.isLoggedIn} /> */}
+      <Header isLoggedIn={props.isLoggedIn} />
 
       <main>
         <h1>Your Notes</h1>
-      {/* {props.notes.length > 0 ? <NoteList notes={props.notes} /> : <p> No Notes Found.</p>} */}
+        {props.notes.length > 0 ? <NoteList notes={props.notes} /> : <NoNotesFound />}
       </main>
       <Footer />
     </>
   );
+}
+
+function NoNotesFound() {
+  return (
+    <div>
+      <p>No notes found in your account.</p>
+      <p>If you <Link href="/create">create a note</Link> you can see them here!</p>
+    </div>
+  )
 }
